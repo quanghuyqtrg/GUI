297dbbe136319ae4bea9dd8c72a0bb315a783b68
quanghuyqtrg <118985487+quanghuyqtrg@users.noreply.github.com>
2023-03-29 20:09:29 +0700

    Update 8.py

--- 8.py
+++ 8.py
@@ -33,13 +33,13 @@ class Hospital:
  # The above code is creating a label with the text "Hospital Management System" and the font size is
  
  
-        lbltitle=Label(self.root,bd=20,relief=RIDGE,text="[PUBLIC GOVERMENTAL SERVICES INFORMATION MANAGEMENT SYSTE]",fg="red",bg="white",font=("times new roman",30,"bold"))
+        lbltitle=Label(self.root,bd=20,relief=RIDGE,text="PUBLIC GOVERMENTAL SERVICES INFORMATION MANAGEMENT SYSTEM",fg="red",bg="white",font=("times new roman",30,"bold"),wraplength=1300)
 
         lbltitle.pack(side=TOP,fill=X)
         
         # Create the Dataframe
         Dataframe=Frame(self.root,bd=20,relief=RIDGE,padx=20)
-        Dataframe.place(x=0,y=130,width=1530,height=400)
+        Dataframe.place(x=0,y=130,width=1540,height=400)
         
         DataframeLeft=LabelFrame(Dataframe,bd=10,relief=RIDGE,padx=10,text="Citizen Information",font=("times new roman",12,"bold"),fg="red")
         DataframeLeft.place(x=0,y=5,width=980,height=350)
@@ -50,11 +50,11 @@ class Hospital:
         ## Create buttons frame
         # Creating a frame for the buttons.
         Buttonframe=Frame(self.root,bd=20,relief=RIDGE,)
-        Buttonframe.place(x=0,y=530,width=1530,height=70)
+        Buttonframe.place(x=0,y=530,width=1540,height=80)
         
         ## create detail frame
         Detailsframe=Frame(self.root,bd=20,relief=RIDGE,)
-        Detailsframe.place(x=0,y=600,width=1530,height=190)
+        Detailsframe.place(x=0,y=600,width=1540,height=190)
         
         ##Create DataframeLeft
         lblName=Label(DataframeLeft,text="Enter date of birth",font=("times new roman",12,"bold"),)
@@ -62,17 +62,15 @@ class Hospital:
         lblName.grid(row=0,column=0,padx=10,pady=10,# Aligning the text to the left.
         sticky=W)
         
-        # Creating a combo box for the day
-        day = ttk.Combobox(DataframeLeft, width=2)
-        day.grid(row=0,column=1,padx=10,pady=10,sticky=W)
+        
 
         # Create a combo box for the month
         month = ttk.Combobox(DataframeLeft, width=10)
-        month.grid(row=0,column=2,padx=10,pady=10,sticky=NW)
+        month.grid(row=0,column=1,padx=10,pady=10,sticky=W )
 
         # Create a combo box for the year
         year = ttk.Combobox(DataframeLeft, width=4)
-        year.grid(row=0,column=3,padx=10,pady=10,sticky=W)
+        year.grid(row=0,column=2,padx=10,pady=2,sticky=W)
 
         # Create a list of days
         days = [i for i in range(1,32)]
@@ -82,7 +80,7 @@ class Hospital:
         years = [i for i in range(1900,2023)]
 
         # Set the values of the combo boxes
-        day['values'] = days
+        
         month['values'] = months
         year['values'] = years
 
@@ -122,22 +120,6 @@ class Hospital:
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         ## Create DataframeRight
         # The above code is creating a text box with the name txtPrescription.
         self.txtPrescription=Text(DataframeRight,font=("arial",12,"bold"),width=46,height=16,padx=2,pady=6)
@@ -148,22 +130,29 @@ class Hospital:
         ## Create Buttonframe
         # The above code is creating a button with the name btnPrescription1 and the text on the
         # button is "Prescription".
-        btnPrescription1 = Button(Buttonframe, text="Prescription", bg="green", fg="black", font=("arial", 12, "bold"), width=23, height=16, padx=2, pady=6)
+       ## Create DataframeRight
+        # The above code is creating a text box with the name txtPrescription.
+        self.txtPrescription=Text(DataframeRight,font=("arial",12,"bold"),width=46,height=16,padx=2,pady=6)
+        
+        self.txtPrescription.grid(row=0,column=0)
+        
+        btnPrescription1 = Button(Buttonframe, text="Prescription", bg="green", fg="white", font=("arial", 12, "bold"), width=25, height=4, padx=2, pady=2,anchor= "center" )
         btnPrescription1.grid(row=0, column=0)
+        
 
-        btnPrescription2 = Button(Buttonframe, text="Prescription Data", bg="green", fg="white", font=("arial", 12, "bold"), width=27, height=16, padx=2, pady=2)
+        btnPrescription2 = Button(Buttonframe, text="Prescription Data", bg="green", fg="white", font=("arial", 12, "bold"), width=27, height=4, padx=2, pady=2,anchor="center")
         btnPrescription2.grid(row=0, column=1)
 
-        btnUpdate = Button(Buttonframe, text="Update", bg="green", fg="white", font=("arial", 12, "bold"), width=27, height=16, padx=2, pady=2)
+        btnUpdate = Button(Buttonframe, text="Update", bg="green", fg="white", font=("arial", 12, "bold"), width=26, height=4, padx=2, pady=2,anchor="center")
         btnUpdate.grid(row=0, column=2)
 
-        btnDelete = Button(Buttonframe, text="Delete", bg="green", fg="white", font=("arial", 12, "bold"), width=27, height=16, padx=2, pady=2)
+        btnDelete = Button(Buttonframe, text="Delete", bg="green", fg="white", font=("arial", 12, "bold"), width=26, height=4, padx=2, pady=2,anchor="center")
         btnDelete.grid(row=0, column=3)
 
-        btnClear = Button(Buttonframe, text="Clear", bg="green", fg="white", font=("arial", 12, "bold"), width=27, height=16, padx=2, pady=2)
+        btnClear = Button(Buttonframe, text="Clear", bg="green", fg="white", font=("arial", 12, "bold"), width=26, height=4, padx=2, pady=2,anchor="center")
         btnClear.grid(row=0, column=4)
 
-        btnExit = Button(Buttonframe, text="Exit", bg="green", fg="white", font=("arial", 12, "bold"), width=27, height=16, padx=2, pady=2)
+        btnExit = Button(Buttonframe, text="Exit", bg="green", fg="white", font=("arial", 12, "bold"), width=30, height=4, padx=2, pady=2,anchor="center")
         btnExit.grid(row=0, column=5)
 
         ## Create tabler
